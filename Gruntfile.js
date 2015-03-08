871c4aa501eb5398042d7a16d94a51e16b9d71b9
Tilman Justen <git@tilmanjusten.de>
2015-03-08 17:55:51 +0100

    Add grunt task for development purposes

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,47 +8,53 @@
 
 'use strict';
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    jshint: {
-      all: [
-        'Gruntfile.js',
-        'tasks/*.js'
-      ],
-      options: {
-        jshintrc: '.jshintrc'
-      }
-    },
+    // Project configuration.
+    grunt.initConfig({
+        clean: ['./inventory'],
 
-    // Configuration to be run (and then tested).
-    'partial-extract': {
-      test: {
-        options: {
-            storePartials: true
+        jshint: {
+            all: [
+                'Gruntfile.js',
+                'tasks/*.js'
+            ],
+            options: {
+                jshintrc: '.jshintrc'
+            }
         },
-        files: [{
-          expand: true,
-          cwd: './test',
-          src: '*.html'
-        }]
-      }
-    }
 
-  });
+        // Configuration to be run (and then tested).
+        'partial-extract': {
+            test: {
+                options: {
+                    storePartials: true
+                },
+                files: [{
+                    expand: true,
+                    cwd: './test',
+                    src: '*.html'
+                }]
+            }
+        }
 
-  // Actually load this plugin's task(s).
-  grunt.loadTasks('tasks');
+    });
 
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-jshint');
+    // Actually load this plugin's task(s).
+    grunt.loadTasks('tasks');
 
-  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
-  // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['jshint', 'partial-extract:test']);
+    // These plugins provide necessary tasks.
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
 
-  // By default, lint and run all tests.
-  grunt.registerTask('default', ['jshint']);
+    // Whenever the "test" task is run, first clean the "tmp" dir, then run this
+    // plugin's task(s), then test the result.
+    grunt.registerTask('test', ['jshint', 'partial-extract:test']);
+
+    // By default, lint and run all tests.
+    grunt.registerTask('default', ['jshint']);
+
+    // development task
+    grunt.registerTask('develop', ['clean', 'partial-extract']);
 
 };
